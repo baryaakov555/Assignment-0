39a7a30dfe768a5b6e6749291e97ab60ce41147c
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-14 20:53:27 -0400

    countOfAllBooleans done

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,15 @@
-function countOfAllBooleans(arr) {
-  // Insert code here;
+function countOfAllBooleans(arr)
+{
+  let num = 0;
+
+  for(i = 0; i < arr.length; i++)
+  {
+    if (typeof arr[i] === 'boolean')
+    {
+      num++
+    } 
+  }
+  return num;
 }
 
 // Do not edit this line;
