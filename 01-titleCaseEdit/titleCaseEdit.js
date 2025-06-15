19a607fc746b18b9d7c07b23fc1ab41a0fc3ffb7
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-14 20:35:38 -0400

    titleCaseEdit  done

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,6 @@
-function titleCaseEdit(title) {
-  // Insert code here;
+function titleCaseEdit(title) 
+{
+  return title.split(' ').map(word => word ? word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() : '').join(' ');
 }
 
 // Do not edit this line;
