551077342218b0930f1503b1f9111565dd271602
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-14 21:10:00 -0400

    countOfAllIndexMatchingNumbers done

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,15 @@
-function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+function countOfAllIndexMatchingNumbers(nums)
+{
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++)
+  {
+    if (nums[i] == i)
+    {
+      count++
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
