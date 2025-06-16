1d26724a087ce55bebae5cb205c33fe6a50f8b62
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-16 17:45:43 -0400

    pairSum done

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,21 @@
-function pairSum(nums, target) {
-  // Insert code here;
+function pairSum(nums, target)
+{
+  if (nums.length <= 1)
+  {
+    throw new Error("Array length cannot be 1 or less");
+  }
+  
+  for (let i = 0; i < nums.length; i++)
+  {
+    for (let j = 0; j < nums.length; j++)
+    {
+      if (i != j && nums[i] + nums[j] == target)
+      {
+        return true
+      }
+    }
+  }
+  return false;
 }
 
 // Do not edit this line;
