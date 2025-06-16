dc217e45b494739f92c8c24ac9e12db537672359
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-16 17:34:34 -0400

    setUnionOfAnyAmountOfSets done

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,15 @@
-function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+function setUnionOfAnyAmountOfSets(...args)
+{
+  let unionSet = new Set();
+
+  for (let i = 0; i < args.length; i++)
+  {
+    for ( let value of args[i])
+    {
+      unionSet.add(value);
+    }
+  }
+  return unionSet;
 }
 
 // Do not edit this line;
