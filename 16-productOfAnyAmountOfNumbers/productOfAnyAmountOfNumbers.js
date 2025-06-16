b9bb6c62ac2cd42e0c64e641eff4aaa94a11db1c
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-16 17:26:06 -0400

    productOfAnyAmountOfNumbers done

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,12 @@
-function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+function productOfAnyAmountOfNumbers(...args)
+{
+  let prod = 1;
+
+  for (let i = 0; i < args.length; i++)
+  {
+    prod = prod * args[i];
+  }
+  return prod;
 }
 
 // Do not edit this line;
